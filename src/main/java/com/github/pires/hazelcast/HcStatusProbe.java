2beabb0f814a350d9061bb2da8cedc63bdd84272
Shaohong <shaohong.li@callstats.io>
2017-08-16 18:24:54 +0300

    minor improvement

--- src/main/java/com/github/pires/hazelcast/HcStatusProbe.java
+++ src/main/java/com/github/pires/hazelcast/HcStatusProbe.java
@@ -147,14 +147,14 @@ public class HcStatusProbe {
 		}
 	}
 
-	@RequestMapping(path = "/shutdownlocal", method = RequestMethod.POST)
+	@RequestMapping(path = "/shutdownlocal")
 	public ResponseEntity<String> shutDownLocalInstance() {
 		if (HcStatusProbe.hcInstance == null) {
 			logger.error("shutDownLocalInstance - hazelcast instance not created yet!");
 			return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
 		} else {
 			logger.warn("shutDownLocalInstance - INITIATED");
-			HcStatusProbe.hcInstance.shutdown();
+			HcStatusProbe.hcInstance.getLifecycleService().shutdown();
 			logger.warn("shutDownLocalInstance - COMPLETED");
 			return new ResponseEntity<String>(HttpStatus.OK);
 		}
